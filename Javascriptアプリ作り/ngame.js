523753733eda55776679f4aa58967616a00e42de
hirokiyuhara <146685461+hirokiyuhara@users.noreply.github.com>
2024-01-26 10:18:53 +0900

    数字の並びを、乱数に変更

--- Javascriptアプリ作り/ngame.js
+++ Javascriptアプリ作り/ngame.js
@@ -3,17 +3,27 @@ let node_Body = document.getElementsByTagName('body');
 let nBody = node_Body.item(0);
 
 let a = [];
-for (i = 1; i <= n * n; i++) {
+for (let i = 1; i <= n * n; i++) {
   a.push(0);
 }
 
-for (x = 0; x < n; x++) {
-  for (y = 0; y < n; y++) {
+for (let x = 0; x < n; x++) {
+  for (let y = 0; y < n; y++) {
     let elmDiv = document.createElement('div');
     let elmP = document.createElement('P');
     elmDiv.style.left = x * 60 + 50 + 'px';
     elmDiv.style.top = y * 60 + 50 + 'px';
-    elmP.textContent = a[y * n + x];
+
+    let r;
+    while (1) {
+      r = Math.floor(Math.random() * n * n);
+      if (a[r] === 0) {
+        a[r] = 1;
+        break;
+      }
+    }
+
+    elmP.textContent = r + 1;
     elmDiv.append(elmP);
     nBody.append(elmDiv);
   }
