0a6b3933897fb4730f1efc585741063b95cfd433
hirokiyuhara <146685461+hirokiyuhara@users.noreply.github.com>
2024-01-26 11:27:41 +0900

    CSSで記述した枠の大きさもコードで記述

--- Javascriptアプリ作り/ngame.js
+++ Javascriptアプリ作り/ngame.js
@@ -1,6 +1,12 @@
 const n = 5;
 let node_Body = document.getElementsByTagName('body');
 let nBody = node_Body.item(0);
+//計算式に従って枠の大きさを算出する
+const m = 0.3;
+let cw = document.documentElement.clientWidth;
+let ch = document.documentElement.clientHeight;
+let s = ch / ((m + 1) * (n + 1) - 1);
+let g = s * m;
 
 let a = [];
 for (let i = 1; i <= n * n; i++) {
@@ -11,8 +17,12 @@ for (let x = 0; x < n; x++) {
   for (let y = 0; y < n; y++) {
     let elmDiv = document.createElement('div');
     let elmP = document.createElement('P');
-    elmDiv.style.left = x * 60 + 50 + 'px';
-    elmDiv.style.top = y * 60 + 50 + 'px';
+    elmDiv.style.left = x * (s + g) + g + 'px';
+    elmDiv.style.top = y * (s + g) + g + 'px';
+
+    //CSSで記述した枠の大きさもコードで記述
+    elmDiv.style.width = s + 'px';
+    elmDiv.style.height = s + 'px';
 
     let r;
     while (1) {
